ad36affb8fa6b82ce26f6464d8f4a34a7ee6baf1
bihanviranga <bihanviranga@gmail.com>
2021-02-03 22:21:28 +0530

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,35 @@
+<script>
+</script>
+
+<style>
+	h1,
+	p {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 2em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Bihan's Blog</title>
+</svelte:head>
+
+<h1>Hello, world!</h1>
+
+<p>This is a simple description about my blog.</p>
