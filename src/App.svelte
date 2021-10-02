3279bdde06cf6917100cf665f9b26478b726c076
Bihan Viranga <bihanviranga@gmail.com>
2021-10-02 23:03:07 +0530

    F: Added navbar placeholder

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,14 @@
 <script>
-	export let name;
+  import Navbar from "./components/Navbar.svelte";
 </script>
 
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
+  />
+</svelte:head>
+
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <Navbar />
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,24 @@
+<script>
+  import {
+    Navbar,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Icon,
+  } from "sveltestrap";
+</script>
+
+<div>
+  <Navbar color="dark" dark expand="md">
+    <NavbarBrand href="/">Bihan's Blog</NavbarBrand>
+    <Nav class="ms-auto" navbar>
+      <NavItem>
+        <NavLink href="https://github.com/bihanviranga">Github</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="#" />
+      </NavItem>
+    </Nav>
+  </Navbar>
+</div>
